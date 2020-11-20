188416d843ffbe8d4847058c5be58be9386fae5a
Terrence <terrencejamesdiaz@gmail.com>
2020-11-19 21:47:21 -0800

    adding console messages to playaudio.js

--- playaudio.js
+++ playaudio.js
@@ -1,14 +1,21 @@
 var fs = require("fs");
 
 function playAudio(bot, VCID, audio) {
+    console.log("Playing audio.");
     bot.joinVoiceChannel(VCID, function(err, events) {
-        bot.getAudioContext(VCID, function(err, stream) {
+        console.log("Joining voice channel.");
         if (err) return console.error(err);
-        stream.on('error', console.log);
-        fs.createReadStream(audio).pipe(stream, {end: false});
-        stream.on('done', function() {
-            bot.leaveVoiceChannel(VCID);
-        });
+        bot.getAudioContext(VCID, function(err, stream) {
+            console.log("Retrieving audio context.");
+            if (err) return console.error(err);
+            stream.on('error', console.log);
+            console.log("Creating read stream.");
+            fs.createReadStream(audio).pipe(stream, {end: false});
+            console.log("Done with read stream.");
+            stream.on('done', function() {
+                console.log("Done, leaving voice channel.");
+                bot.leaveVoiceChannel(VCID);
+            });
         });
     });
 }
